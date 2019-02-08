602bc7df3f0e57bfdc09119fa893ad32668f4eeb
Jere Suikkila <jere@suikkila.com>
2019-02-08 09:40:12 +0200

    Initial game model

--- movesandgames/settings.py
+++ movesandgames/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'prototype.apps.PrototypeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- prototype/models.py
+++ prototype/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    video_link = models.URLField()
+    rules = models.TextField()
+    player_count = models.CharField(max_length=200)
+
+# - description
+# - video
+# - photos
+# - rules
+# - player count
+# - 
